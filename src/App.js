e2cccc680920445718beccc12281132e50301e67
samspatel07 <sumitpatel787491@gmail.com>
2023-06-06 14:49:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
 import logo from './logo.svg';
 import './css/App.css';
-import HomePage from './Pages/HomePage';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Cart from './Pages/Cart';
-import NotFoundPage from './Pages/NotFoundPage';
+import EditUser from './Pages/EditUser';
 import globalStyles from './Components/Constants';
 import appStyle from './css/AppStyle.module.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './Components/Header';
-
+import { AuthProvider, useAuthContext } from './contexts/auth';
+import LoginProvider from './contexts/Loginprovider';
+import { AuthContext } from './contexts/auth';
+import { useContext } from 'react';
+import Footer from './Components/Footer';
+import UpdateProfile from './Pages/UpdateProfile';
+import { loginContext } from './contexts/LoginContext';
+import loader from '../src/assets/Loader-Icon.gif';
+import '../src/css/loader.css';
+import { MainNavigation } from './MainNavigation';
+import { CartProvider } from './contexts/cartContext';
+// import { MainNavigation } from './Components/MainNavigation';
 
 
 function App() {
-  // const authcontext=useAuthContext();
-  // const Redirect=<Navigate to='/login'/>;
+  // const authContext = useAuthContext();
+  
   return (
     <>
       
       <ToastContainer />
       <BrowserRouter>
-        {/* <div
-          // style={{...globalStyles.navbar}}
-          className={appStyle.navbar}
-        >
-          <Link to='/' style={{ marginLeft: 5 }} className='link'>Home</Link>
-          <Link to='/login' style={{ marginLeft: 10 }} className='link'>Login</Link>
-          <Link to='/register' style={{ marginLeft: 15 }} className='link'>Register Now</Link>
-          <Link to='/error' style={{ marginLeft: 20 }} className='link'>NotFoundPage</Link>
-        </div> */}
+      <LoginProvider>
+        <AuthProvider>
+        <CartProvider>
+        <div>
+        <div className="loader-wrapper">
+                <img src={loader} alt="loader" />
+        </div>
+        </div>
         <Header />
-       
-        <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-          <Route path='/cart' element={<Cart />}></Route>
-          <Route path='*' element={<NotFoundPage />}></Route>
-        </Routes>
+        {/* {JSON.stringify(useAuthContext().user)} */}
+        <MainNavigation/>
+        <Footer/>
+        </CartProvider>
+        </AuthProvider>
+        </LoginProvider>
       </BrowserRouter>
+     
+      
+      
+      
       
     </>);
 
--- src/Components/accessLocalStorage.js
+++
@@ -1,6 +0,0 @@
-const setLocalStorage=(key,data)=>{
-    localStorage.setItem(key,JSON.stringify(data));
-}
-const getLocalStorage=(key)=>{
-    return JSON.parse(localStorage.getItem(key));
-}
